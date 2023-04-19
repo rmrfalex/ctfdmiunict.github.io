69fdce18d7ae9d52f5028c6bb0f740aabf65e2ea
makapx <parlavecchiom98@gmail.com>
2023-04-19 21:42:47 +0200

    feat: rebranding

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-function displayText(str) {
-    n = n + 1;
-    p.innerHTML = "> " + str.slice(0, n);
-    if (n === str.length) {
-      clearInterval(typeTimer);
-      //const node = document.createElement("div");
-      p.innerHTML = "> " + str;
-      n = 0;
-      setInterval(function() {
-  
-        if (n === 0) {
-          p.innerHTML = "> " + str + "▮"
-          //mainHead.appendChild(node);
-          //cursor.classList.add("hidden")
-          n = 1;
-        } else {
-          p.innerHTML = "> " + str
-          //mainHead.removeChild(node);
-          //cursor.classList.remove("hidden")
-          n = 0;
-        };
-      }, 500);
-    };
+/**
+ * @description This function will show the navbar logo when the user scrolls down the page.
+ * 
+ * @returns {void}
+ */
+const showNavbarLogoOnScroll = () => {
+  const navbarLogo = document.getElementsByClassName("navbar__logo")[0];
+  if ( navbarLogo === undefined || navbarLogo === null){
+    return;
   }
 
-let p = document.getElementById('typeText');
-p.innerHTML = '';
-let n = 0;
-let str = 'One team is red, the other is blue. To capture the flag, we need you!';
+  if (window.scrollY > 150){
+    navbarLogo.style.display = "block";
+    return;
+  }
 
-let typeTimer = setInterval(function(){displayText(str);}, 60);
+  navbarLogo.style.display = "none";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("scroll", showNavbarLogoOnScroll);
+});
