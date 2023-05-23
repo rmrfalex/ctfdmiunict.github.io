f4192c72e21e0db25db5fed10668d58463bfa0e2
makapx <martina.parlavecchio@ctmobi.it>
2023-05-23 17:56:44 +0200

    feat: countdown + set event date

--- script.js
+++ script.js
@@ -17,6 +17,35 @@ const showNavbarLogoOnScroll = () => {
   navbarLogo.style.display = "none";
 };
 
+const createCountdown = () => {
+  const countdown = document.getElementById("countdown");
+  if (countdown === undefined || countdown === null){
+    return;
+  }
+
+  const eventDate = new Date("June 12, 2023 00:00:00").getTime();
+
+  setInterval(() => {
+    let currentDate = new Date().getTime();
+    let distance = eventDate - currentDate;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    countdown.innerHTML =
+    "Mancano <b>" + days + "GG " + hours + "H "
+    + minutes + "M " + seconds + "S </b> all'evento";
+
+    if (distance < 0) {
+      clearInterval(x);
+      countdown.innerHTML = "Affrettati!Stiamo iniziando!";
+    }
+  }, 1000);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("scroll", showNavbarLogoOnScroll);
+  createCountdown();
 });
